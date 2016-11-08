3c0d44c2dadd7b094902d0cc1007f40322688863
Joscelyn Jean <Joscelyn.Jean@logibec.com>
2016-11-08 09:34:07 -0500

    Added chat

---
+++ src/chat-server/chat-server/Hubs/ConversationHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using chat_server.Models;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace chat_server.Hubs
+{
+    [HubName("conversation")]
+    public class ConversationHub : Hub
+    {
+        public void ReceiveMessage(MessageInformation messageInformation)
+        {
+            // Send update of conversation to anyone
+            Clients.All.updateConversation(messageInformation);
+        }
+    }
+}
---
+++ src/chat-server/chat-server/Models/MessageInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chat_server.Models
+{
+    public class MessageInformation
+    {
+        public String Message { get; set; }
+    }
+}
